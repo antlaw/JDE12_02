133d211feafa154b8b2d82883e00965489783c2c
KwanHo-Gen <KwanH@genstudents.org>
2025-04-24 15:57:28 +0800

    kwan test

--- group1.py
+++ group1.py
@@ -1,6 +1,6 @@
 import random
 
-text = './news.txt'
+text = 'news.txt'
 with open(text, "r") as f:
     paragraph = f.read()
 
@@ -8,38 +8,28 @@ def hammer_task_0():
     teamJDE = ['hammer', 'billy', 'chistina']
     result = random.sample(teamJDE, 1)
     return result
- 
- # Task-1 - count the total number of words in the prargraph that contains vowel characters(a, e, i, o u)
+
 def memberOne(text):
     vowels = set('aeiouAEIOU')
     words = text.split()
     vowel_count = sum(1 for word in words if any(char in vowels for char in word))
     return f"Words with vowels: {vowel_count}"
 
-
-# Task-2 - encode the paragraph by shifting the position of each character by a variable value e.g. I am a boy (1) -> J bn b cpz
-def memberTwo(text):
-    shift = 1
-    encoded = []
+def memberTwo(text, shift=1):
+    encoded = ''
     for char in text:
         if char.isalpha():
             base = ord('A') if char.isupper() else ord('a')
-            shifted_char = chr((ord(char) - base + shift) % 26 + base)
-            encoded.append(shifted_char)
+            encoded += chr((ord(char) - base + shift) % 26 + base)
         else:
-            encoded.append(char)
-    return ''.join(encoded)
+            encoded += char
+    return encoded
 
-
-# Task-3 - Reverse the entire paragraph line by line e.g. I am a boy -> yob a ma I
 def memberThree(text):
     lines = text.split('\n')
     reversed_lines = [line[::-1] for line in lines]
     return '\n'.join(reversed_lines)
 
-
-
-# Task-4 - Reverse the order of character of each word e.g. I am a boy -> I ma a yob
 def memberFour(text):
     words = text.split()
     reversed_words = [word[::-1] for word in words]
@@ -61,3 +51,5 @@ if __name__ == "__main__":
     print('call memberTwo():', memberTwo(paragraph))
     print('call memberThree():', memberThree(paragraph))
     print('call memberFour():', memberFour(paragraph))
+
+# test push (Kwan)
